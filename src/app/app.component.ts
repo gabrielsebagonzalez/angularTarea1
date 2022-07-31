e47a0f2a354ce7b1d6520b09ee98f917aa1dcd4b
sebagonzalez <gabrielsebastian@gmail.com>
2022-07-30 22:52:21 -0300

    Muestro una lista de usuarios y un boton con angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
   title = 'tarea1';
+
+  boton = false
+
+  usuarios: Usuarios [] = [
+    {nombre : "Juan" , costos: "2500"},
+    {nombre: "Carlos" , costos: "3000"},
+    {nombre: "Cecilia", costos: "4500"},
+
+  ];
+
+  ocultarUsuarios() {
+    this.usuarios = []
+    this.boton = true
+  }
+
+
 }
+
+interface Usuarios {
+  nombre : string;
+  costos : string;
+}
+
+
+
+
+
+
+
+
+
+
+
